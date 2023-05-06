882e8f9deb939c6c3332ec70a079df3dedf1a7fe
Cafe com Codigo <esttstart@gmail.com>
2023-05-06 19:40:17 -0300

    instalando theme changer

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,7 +1,15 @@
 <script>
-//   import imgUrl from "../assets/logo.png";
-import Whatsapp from "./whatsapp.svelte";
-  import { page } from "$app/stores";
+  //   import imgUrl from "../assets/logo.png";
+  import Whatsapp from "./whatsapp.svelte";
+  import { onMount } from "svelte";
+
+  import { themeChange } from "theme-change";
+
+  // NOTE: the element that is using one of the theme attributes must be in the DOM on mount
+  onMount(() => {
+    themeChange(false);
+    // 👆 false parameter is required for svelte
+  });
 </script>
 
 <div class="navbar fixed bg-base-100 z-40">
@@ -65,8 +73,46 @@ import Whatsapp from "./whatsapp.svelte";
       </li>
     </ul>
     <div class="pl-10">
-			<Whatsapp />
-		</div>
+      <Whatsapp />
+    </div>
+    <div class="mx-4">
+      <select
+        name="themes"
+        id="themes"
+        data-choose-theme
+        class="select select-ghost w-full max-w-xs"
+      >
+        <option value="light">Default</option>
+        <option value="dark">Dark</option>
+        <option value="cupcake">Cupcake</option>
+        <option value="bumblebee">Bumblebee</option>
+        <option value="emerald">Emerald</option>
+        <option value="corporate">Corporate</option>
+        <option value="synthwave">Synthwave</option>
+        <option value="retro">Retro</option>
+        <option value="cyberpunk">Cyberpunk</option>
+        <option value="valentine">Valentine</option>
+        <option value="halloween">Halloween</option>
+        <option value="garden">Garden</option>
+        <option value="forest">Forest</option>
+        <option value="aqua">Aqua</option>
+        <option value="lofi">Lofi</option>
+        <option value="pastel">Pastel</option>
+        <option value="fantasy">Fantasy</option>
+        <option value="wireframe">Wireframe</option>
+        <option value="black">Black</option>
+        <option value="luxury">Luxury</option>
+        <option value="dracula">Dracula</option>
+        <option value="cmyk">CMYK</option>
+        <option value="autumn">Autumn</option>
+        <option value="business">Business</option>
+        <option value="acid">Acid</option>
+        <option value="lemonade">Lemonade</option>
+        <option value="night">Night</option>
+        <option value="coffee">Coffee</option>
+        <option value="winter">Winter</option>
+      </select>
+    </div>
   </div>
   <div class="navbar-end" />
 </div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
-
+import { vitePreprocess } from "@sveltejs/kit/vite";
+import adapter from "@sveltejs/adapter-static";
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: "build",
+      assets: "build",
+      fallback: null,
+      precompress: false,
+      strict: false,
+    }),
+    inlineStyleThreshold: Infinity,
   },
+  // seo: {
+  // 	title: 'Cafe com Codigo',
+  // 	description:
+  // 		'Empresa que cria sites para profissionais da saúde, criamos landingpage, site institucional, site completo com blog, robôs para atendimento whatsapp',
+  // 	keywords: 'criar site, site institucional, landingpage, blog, robô whatsapp'
+  // },
+  preprocess: vitePreprocess(),
 };
 
 export default config;
